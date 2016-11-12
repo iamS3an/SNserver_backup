ff2e3e63eceae7b0bf739b100162687223c1dc4f
seanlin2014 <seanlin12345@gmail.com>
2016-11-12 10:13:18 +0800

    webhook set up

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hi,你好' });
 });
 
+router.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'snstudio666') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 router.get('/interface', function(req, res, next) {
   res.render('interface', { title: 'Express' });
 });
