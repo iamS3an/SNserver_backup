34f6be2b912241d1179204f78d522c87b841f16a
seanlin2014 <seanlin12345@gmail.com>
2016-12-10 08:42:33 +0800

    just upload

--- daemon/fbChatBot.js
+++ daemon/fbChatBot.js
@@ -3,16 +3,16 @@ var login = require("facebook-chat-api");
 const fbBot = () => {
   login({email: "qsnstudioq@gmail.com", password: "1qa2ws3ed"}, function callback (err, api) {
     if(err) {
-      setInterval(markAsRead(), 5 * 1000);
+      setInterval(fbBot(), 5 * 1000);
       return console.error(err);
     }
 
     api.setOptions({
       forceLogin: true
     });
-
+    
     api.listen(function callback(err, message) {
-      // api.markAsRead(message.threadID);
+      api.markAsRead(message.threadID);
       if(message.body == "time") {
         api.sendMessage(Date(), message.threadID);
       }
