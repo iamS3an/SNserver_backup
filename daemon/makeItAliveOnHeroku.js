18c65d6ebd0430290156bdb4fe266ad463bb66c0
seanlin2014 <seanlin12345@gmail.com>
2016-12-12 21:22:46 +0800

    hi

--- daemon/makeItAliveOnHeroku.js
+++ daemon/makeItAliveOnHeroku.js
@@ -4,7 +4,7 @@ var schedule = require('node-schedule');
 schedule.scheduleJob('1 1,11,21,31,41,51 * * * *', function(){
   console.log('The answer to life, the universe, and everything!' + new Date());
   http.get({
-        host: 'snfbbot.herokuapp.com',
+        host: 'herokuwakeup.herokuapp.com',
         path: '/'
   }, function(response) {
         // Continuously update stream with data
