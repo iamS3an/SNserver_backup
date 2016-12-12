c0880f2d10a951782fe769c899235f1aa9f51c07
seanlin2014 <seanlin12345@gmail.com>
2016-12-12 21:52:55 +0800

    fix bug

--- daemon/fbChatBot.js
+++ daemon/fbChatBot.js
@@ -15,10 +15,12 @@ const fbBot = () => {
       api.markAsRead(message.threadID);
       if(message.body == "@") {
         api.sendMessage("請問你要查詢：\n(1)大耳朵的戰績\n(2)其他", message.threadID);
-        if(message.body == "1" || "(1)") {
+      }
+      else {
+        if(message.body == "1") {
           api.sendMessage("還沒做好XD", message.threadID);
         }
-        else {
+        else if(message.body == "2") {
           api.sendMessage("目前暫不開放", message.threadID);
         }
       }
